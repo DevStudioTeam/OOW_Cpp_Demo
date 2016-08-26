e98f272871ef49339bc69b2d33492de357973280
Ilia Gromov <igromovbox@gmail.com>
2016-08-26 11:15:09 +0300

    Initial commit

--- main.cpp
+++ main.cpp
@@ -4,9 +4,12 @@
 #include <errno.h>
 #include <sys/utsname.h>
 
+#include <stdio.h> 
+#include <math.h>
+
 using namespace std;
 
-int main(void) {
+int print_uname() {
 
     struct utsname sysinfo;
 
@@ -22,10 +25,20 @@ int main(void) {
     cout << "Kernel Build Timestamp: " << sysinfo.version << endl;
     cout << "Machine Arch: " << sysinfo.machine << endl;
 
-
 #ifdef _GNU_SOURCE
     cout << "Domain Name: " << sysinfo.domainname << endl;
 #endif
 
     return EXIT_SUCCESS;
+}
+
+void error_prone() {
+    char buffer[10];
+//    sprintf(buffer, "The message is %s", "Hello World! I'm presenting you a typical overflow");
+    snprintf(buffer, sizeof (buffer), "%s", "Hello World! No buffer issues here");
+}
+
+int main() {
+    print_uname();
+    error_prone();
 }
